f1196f57675da1a1bcdfd05597ed4f16904501ff
Titi TUNTIYANUKULCHAI <titi.tuntiyanukulchai@scb.co.th>
2020-09-26 16:53:52 +0700

    add firestore

--- app/src/main/java/com/example/testfirestore/MainActivity.kt
+++ app/src/main/java/com/example/testfirestore/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.testfirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
     }
 }
